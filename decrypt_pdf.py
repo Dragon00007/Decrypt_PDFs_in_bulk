458391663a8c27420fe8f6d82ebc3370e24280fc
Dragon00007 <60709793+Dragon00007@users.noreply.github.com>
2020-12-31 17:09:50 +0530

    Add files via upload

---
+++ decrypt_pdf.py
@@ -0,0 +1,47 @@
+'''
+    Requirement:
+                pip install pikepdf
+'''
+import pikepdf
+import os
+
+'''
+    For the given path, get the List of all PDF files in the directory tree
+                                    and
+                                unlock them
+'''
+dirName = input('please input the path to the directory of the PDFs:\n')
+Password = input('please input the password:\n')
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+    return allFiles
+
+
+def main():
+    global dirName
+    global Password
+    # Get the list of all files in directory tree at given path
+    listOfFiles = getListOfFiles(dirName)
+    # Print the files
+    for elem in listOfFiles:
+        p = pikepdf.open(elem, password=Password, allow_overwriting_input=True)
+        p.save(elem)
+        print(elem)
+    print("****************")
+
+
+if __name__ == '__main__':
+    main()
